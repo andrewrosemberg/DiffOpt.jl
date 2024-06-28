ac8a2394f61b9d3b5caed149e29ddccd752982b3
andrewrosemberg <andrewrosemberg@gmail.com>
2024-06-28 15:27:15 -0600

    add dense jacobian

--- testing_barrier.jl
+++ testing_barrier.jl
@@ -18,15 +18,16 @@ model = Model(Ipopt.Optimizer)
 
 # Parameters
 @variable(model, p ∈ MOI.Parameter(1.0))
+@variable(model, p2 ∈ MOI.Parameter(2.0))
 
 # Variables
 @variable(model, x) 
 @variable(model, y)
 
 # Constraints
-@constraint(model, con1, y == sin(p)) # NLP Constraint
-@constraint(model, con2, x >= p)
-@constraint(model, con3, y >= 0)
+@constraint(model, con1, y >= p*sin(x)) # NLP Constraint
+@constraint(model, con2, x + y == p)
+@constraint(model, con3, p2 * x >= 0.1)
 @objective(model, Min, (1 - x)^2 + 100 * (y - x^2)^2) # NLP Objective
 optimize!(model)
 
@@ -47,10 +48,18 @@ function _dense_hessian(hessian_sparsity, V, n)
     )
 end
 
+function _dense_jacobian(jacobian_sparsity, V, m, n)
+    I = [i for (i, j) in jacobian_sparsity]
+    J = [j for (i, j) in jacobian_sparsity]
+    raw = SparseArrays.sparse(I, J, V, m, n)
+    return Matrix(raw)
+end
+
 # Primal Solution
 primal_values = value.([x, y])
 dual_values = dual.([con1; con2; con3])
 num_vars = length(primal_values)
+num_cons = length(dual_values)
 
 # `Evaluator`: Object that helps evaluating functions and calculating related values (Hessian, Jacobian, ...)
 evaluator = JuMP.MOI.Nonlinear.Evaluator(model.moi_backend.optimizer.model.nlp_model, JuMP.MOI.Nonlinear.SparseReverseMode(), [model.moi_backend.model_to_optimizer_map[index(x)], model.moi_backend.model_to_optimizer_map[index(y)]])
@@ -59,8 +68,8 @@ evaluator = JuMP.MOI.Nonlinear.Evaluator(model.moi_backend.optimizer.model.nlp_m
 MOI.initialize(evaluator, [:Grad, :Jac, :Hess, :JacVec])
 
 # Hessian "Symetric" structure values - Placeholder that will be modified to during the evaluation of the hessian
-W = [NaN, NaN, NaN]
 hessian_sparsity = MOI.hessian_lagrangian_structure(evaluator)
+W = fill(NaN, length(hessian_sparsity))
 
 # Modify H with the values for the hessian of the lagrangian
 MOI.eval_hessian_lagrangian(evaluator, W, primal_values, 1.0, dual_values)
@@ -72,6 +81,7 @@ A = zeros(length(jacobian_sparsity))
 
 # Evaluate Jacobian
 MOI.eval_constraint_jacobian(evaluator, A, primal_values)
+A = _dense_jacobian(jacobian_sparsity, A, num_cons, num_vars)
 
 # TODO: ∇ₓₚL (Partial second derivative of the lagrangian wrt primal solution and parameters) ; 
 # TODO: ∇ₚC (partial derivative of the equality constraintswith wrt parameters).
@@ -81,8 +91,8 @@ MOI.eval_constraint_jacobian(evaluator, A, primal_values)
 
 # Calculate Sensitivity
 ############
-V = diag(dual_values)
-X = diag(primal_values)
+V = diagm(dual_values)
+X = diagm(primal_values)
 
 M = [
     [W A -I];
